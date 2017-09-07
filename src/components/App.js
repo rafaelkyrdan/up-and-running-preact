5714ff1d36bf55a0424dd8326098f5c05b4cb718
rafaelkyrdan <rafaelkyrdan@gmail.com>
2017-09-07 13:25:03 +0300

    define stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { h, preact } from 'preact';
+import { h, preact, Component } from 'preact';
 import User from './User';
 
 const users = [
@@ -6,6 +6,42 @@ const users = [
 	{image: 'https://avatars2.githubusercontent.com/u/15754928?v=4&s=460', name: 'Jaroslav Shkarupilo'}
 ]
 
+export class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+			loading: true
+		}
+	}
+
+	componentDidMount() {
+		fetch(this.props.config.urls.user)
+			.then(resp => resp.json())
+			.then(user => {
+				console.log(user);
+				this.setState({
+					users: [user],
+					loading: false	
+				})
+			})
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		return (
+			<div class="app">
+				<h1>Hello world!</h1>
+				{this.state.loading
+					? <p>Please wait ...</p>
+					: this.state.users.map(user => <User name={user.name} image={user.avatar_url} key={user.name} />)
+				}								
+			</div>
+		);
+	}	
+}
+
+/*
 export function App() {
 	return (
 		<div class="app">
@@ -14,5 +50,6 @@ export function App() {
 		</div>
 	);
 }
+*/
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { h, render } from 'preact';
 import App from './components/App';
 
-render(<App />, document.querySelector('main'));
+const config = {
+	urls: {
+		user: "https://api.github.com/users/rafaelkyrdan"
+	}
+}
+render(<App config={config} />, document.querySelector('main'));
 console.log('index.js');
